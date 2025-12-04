662868308ea63259a48a3fcaa4749508a2864d08
band_rose_59 <2023bit027@sggs.ac.in>
2025-12-04 10:53:22 +0530

    Update index.ts

--- src/config/about/index.ts
+++ src/config/about/index.ts
@@ -1,5 +1,5 @@
 export const aboutConfig = {
-  title: 'What is HackNXT?',
+  title: 'What is HackNXT 1.0?',
   description:
     "HackNXT is a 36-hour hackathon hosted at SGGS Nanded, designed for engineers and tech enthusiasts to build real-world solutions under pressure. With 500+ participants, 50+ teams, industry mentors, and non-stop innovation, HackNXT pushes you to execute fast, think sharp, and ship projects that actually matter.",
 };
